d447bfe23e836caaae66ac770529b351a6fd71d9
Lucas Leblow <leblowl@gmail.com>
2015-06-13 20:09:54 +0200

    actual deploy.sh in place

--- rsc/deploy.sh
+++ rsc/deploy.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-docker ps
+/usr/bin/docker pull zoondka/zoondka-maps
+/usr/bin/docker stop zoondka-maps
+/usr/bin/docker rm zoondka-maps
+/usr/bin/docker run -d -p 8090:8090 --name zoondka-maps -t zoondka/zoondka-maps
 
 exit 0
