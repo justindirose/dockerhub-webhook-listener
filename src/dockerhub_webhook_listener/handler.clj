7cf5fe696093a4bfe1a821de8713411e0a4d963c
Justin DiRose <dirose.justin@gmail.com>
2016-06-20 19:53:50 -0500

    Removed IP check
    Removed IP check code since Docker Hub no longer has publicly listed IPs

--- src/dockerhub_webhook_listener/handler.clj
+++ src/dockerhub_webhook_listener/handler.clj
@@ -6,26 +6,11 @@
             [compojure.route :as route]
             [ring.util.response :as resp]))
 
-(defn ip->int [ip-str]
-  (->> (str/split ip-str #"\.")
-       (reverse)
-       (map-indexed (fn [i x]
-                      (-> (Integer. x)
-                          (bit-shift-left (* 8 i)))))
-       (reduce bit-or)))
-
-(def dockerhub-low (ip->int "162.242.195.64"))
-(def dockerhub-high (ip->int "162.242.195.127"))
-
-(defn from-dockerhub? [ip-str]
-  (<= dockerhub-low (ip->int ip-str) dockerhub-high))
-
 (defn token-valid? [token]
   (= (System/getenv "DOCKERHUB_TOKEN") token))
 
 (defn req-valid? [req]
-  (and (from-dockerhub? (:remote-addr req))
-       (token-valid? (get-in req [:params "token"]))))
+  (and (token-valid? (get-in req [:params "token"]))))
 
 (defn deploy []
   (->> (sh "rsc/deploy.sh")
